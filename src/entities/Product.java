7b0824b0d0456504654171bcf42bd6367a3f29f3
Igor Oliveira <igoortech@gmail.com>
2020-01-02 17:45:58 -0300

    Exemplo2 POO iniciado.

---
+++ src/entities/Product.java
@@ -0,0 +1,21 @@
+package entities;
+
+public class Product {
+
+	public String name;
+	public double price;
+	public int quantity;
+	
+	public double totalValueInStock() {
+		return price * quantity;
+	}
+	
+	public void addProducts(int quantity) {
+		this.quantity += quantity; 	/* 'this' EXPLICITA QUE O ACESSO
+		 							É NO ATRIBUTO DA CLASSE 'public int quantity' */
+		}
+	
+	public void removeProducts(int quantity) {
+		this.quantity -= quantity;
+	}
+}
